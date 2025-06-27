870fa992f6d31785c3bf99c9cb64b061213ea70e
Tae Hee Jang <kancth03@gmail.com>
2025-06-27 21:40:45 +0900

    Init

---
+++ src/main/kotlin/com/kancth/openknot/OpenKnotApplication.kt
@@ -0,0 +1,11 @@
+package com.kancth.openknot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenKnotApplication
+
+fun main(args: Array<String>) {
+	runApplication<OpenKnotApplication>(*args)
+}
---
+++ src/test/kotlin/com/kancth/openknot/OpenKnotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kancth.openknot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpenKnotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
